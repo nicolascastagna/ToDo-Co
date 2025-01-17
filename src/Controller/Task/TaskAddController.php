eeda7d2c43a0ce6151bc6e696478fcb7d0bcd195
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2025-01-17 14:35:36 +0100

    auto-assign auth user to tasks + set anonymous for existing

--- src/Controller/Task/TaskAddController.php
+++ src/Controller/Task/TaskAddController.php
@@ -9,13 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 
 class TaskAddController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-    ) {
-    }
+        private readonly Security $security,
+    ) {}
 
     #[Route(path: '/tasks/create', name: 'task_create', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function create(Request $request): Response
@@ -26,6 +27,9 @@ class TaskAddController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->security->getUser();
+            $task->setUser($user);
+
             $this->entityManager->persist($task);
             $this->entityManager->flush();
 
